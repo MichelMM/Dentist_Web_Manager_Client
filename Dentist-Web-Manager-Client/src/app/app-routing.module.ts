352128449d455f6c14b674ae25d1eff746bfb594
Michel Maris Mora <39529432+MichelMM@users.noreply.github.com>
2020-11-04 18:23:09 -0600

    Added NavBar and Home components, they work!

--- Dentist-Web-Manager-Client/src/app/app-routing.module.ts
+++ Dentist-Web-Manager-Client/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {NavBarComponent} from './components/nav-bar/nav-bar.component';
+import {HomeComponent} from './pages/home/home.component'
+
+const routes: Routes = [  
+  { path: '', redirectTo: 'Home', pathMatch: 'full' },
+  { path: 'Home', component: HomeComponent }/*,
+  { path: '**', component: Page404Component};*/
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
