7b87de17ad7cea7f2b0658f8e53c7cbbd1953518
Michel Maris Mora <39529432+MichelMM@users.noreply.github.com>
2020-11-04 18:39:40 -0600

    Added About and Services components

--- Dentist-Web-Manager-Client/src/app/app-routing.module.ts
+++ Dentist-Web-Manager-Client/src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import {NavBarComponent} from './components/nav-bar/nav-bar.component';
-import {HomeComponent} from './pages/home/home.component'
+import {HomeComponent} from './pages/home/home.component';
+import {AboutComponent} from './pages/about/about.component';
+import {ServicesComponent} from './pages/services/services.component';
 
 const routes: Routes = [  
   { path: '', redirectTo: 'Home', pathMatch: 'full' },
-  { path: 'Home', component: HomeComponent }/*,
+  { path: 'Home', component: HomeComponent },
+  { path: 'About', component: AboutComponent },
+  { path: 'Services', component: ServicesComponent },/*,
   { path: '**', component: Page404Component};*/
 ]
 
--- Dentist-Web-Manager-Client/src/app/app.module.ts
+++ Dentist-Web-Manager-Client/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ServicesComponent } from './pages/services/services.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
