37735b851322d27c641d37f3679ed98f8c8479c5
casillas-m <si715612@iteso.mx>
2020-11-04 18:51:53 -0600

    Added routes Contact, galley and faq

--- Dentist-Web-Manager-Client/src/app/app-routing.module.ts
+++ Dentist-Web-Manager-Client/src/app/app-routing.module.ts
@@ -3,11 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 
 import {NavBarComponent} from './components/nav-bar/nav-bar.component';
 import {HomeComponent} from './pages/home/home.component'
+import {FaqComponent} from './pages/faq/faq.component'
+import {GalleryComponent} from './pages/gallery/gallery.component'
+import {ContactComponent} from './pages/contact/contact.component'
 
 const routes: Routes = [  
   { path: '', redirectTo: 'Home', pathMatch: 'full' },
-  { path: 'Home', component: HomeComponent }/*,
-  { path: '**', component: Page404Component};*/
+  { path: 'Home', component: HomeComponent },
+  { path: 'FAQ', component: FaqComponent },
+  { path: 'Gallery', component: GalleryComponent },
+  { path: 'Contact', component: ContactComponent }
+  // { path: '**', component: Page404Component};
 ]
 
 @NgModule({
--- Dentist-Web-Manager-Client/src/app/app.module.ts
+++ Dentist-Web-Manager-Client/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { HomeComponent } from './pages/home/home.component';
+import { FaqComponent } from './pages/faq/faq.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    HomeComponent
+    HomeComponent,
+    FaqComponent,
+    GalleryComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
