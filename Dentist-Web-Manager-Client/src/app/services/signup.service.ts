969665855d6ca6968338e0284281a275814dc6f0
casillas-m <si715612@iteso.mx>
2020-11-19 18:01:04 -0600

    Added signup service

---
+++ Dentist-Web-Manager-Client/src/app/services/signup.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {​​ HttpClient }​​ from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+import { environment } from "../../environments/environment"
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SignupService {
+
+  constructor(private http:HttpClient) { }
+
+  signup(body:any):Promise<any>{
+    body = JSON.stringify({data:body})
+    const httpOptions = {
+      headers: new HttpHeaders({'Content-Type':  'application/json'})
+    }
+    return this.http.post(`${environment.API_URL}/patient`,body,httpOptions).toPromise()
+  }
+
+}
