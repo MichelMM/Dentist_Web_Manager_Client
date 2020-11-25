cbcd34ea639d32d1aa4c56e6a8421864ba9d6856
Miguel Ruvalcaba <55141980+Ruvo99@users.noreply.github.com>
2020-11-24 19:11:45 -0600

    Added the Socket URL for easier usage when needed

--- Dentist-Web-Manager-Client/src/environments/environment.ts
+++ Dentist-Web-Manager-Client/src/environments/environment.ts
@@ -5,6 +5,7 @@
 export const environment = {
   production: false,
   API_URL: "http://localhost:3000/api",
+  SOCKET_URL: "http://localhost:3000",
   clientId: "879994592250-47fqcmat6s2k7dh0n2om8hvgr6q1960a.apps.googleusercontent.com"
 };
 
