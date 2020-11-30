5b3223968a43b845cd88e434eebeac2c3748fb05
casillas-m <si715612@iteso.mx>
2020-11-30 01:23:02 -0600

    Google auth change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   API_URL: "https://med-entist-app.herokuapp.com/api",
   SOCKET_URL: "https://med-entist-app.herokuapp.com",
-  clientId: "879994592250-47fqcmat6s2k7dh0n2om8hvgr6q1960a.apps.googleusercontent.com"
+  clientId: "879994592250-b403daf3er7687ihq64p778rue8chfla.apps.googleusercontent.com"
 };
